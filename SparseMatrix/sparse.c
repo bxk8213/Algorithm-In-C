75cd2455b1fe0222341926045a2f48dc9c886969
Bardan Karki <bxk8213@mavs.uta.edu>
2025-08-18 12:06:55 -0500

    Implemented addition on Sparse Matrix

--- SparseMatrix/sparse.c
+++ SparseMatrix/sparse.c
@@ -50,11 +50,67 @@ void display(struct Sparse s)
     }
 }
 
+struct Sparse * add(struct Sparse *s1, struct Sparse *s2)
+{
+    if(s1->m != s2->m || s1->n != s2->n){
+        return 0;
+    }
+
+    struct Sparse *sum;
+    sum = (struct Sparse *) malloc(sizeof(struct Sparse));
+    
+    sum->e = (struct Element *) malloc((s1->num + s2->num) * sizeof(struct Element));
+
+    int i = 0, j = 0, k = 0;
+
+    while(i < s1->num && j < s2->num)
+    {
+        if(s1->e[i].i < s2->e[j].i){
+            sum->e[k++] = s1->e[i++];
+        }else if(s1->e[i].i > s2->e[j].i){
+            sum->e[k++] = s2->e[j++];
+        }else if(s1->e[i].j < s2->e[j].j) {
+                sum->e[k++] = s1->e[i++];
+        }else if(s1->e[i].j > s2->e[j].j){
+            sum->e[k++] = s2->e[j++];
+        }else{
+
+            sum->e[k] = s1->e[i];
+            sum->e[k++].x = s1->e[i++].x + s2->e[j++].x;
+        }
+    }
+
+    for(; i < s1->num; i++){
+        sum->e[k++] = s1->e[i];
+    }
+
+    for(; j < s2->num; j++){
+        sum->e[k++] = s2->e[j];
+    }
+
+    sum->m = s1->m;
+    sum->n = s1->n;
+    sum->num = k;
+    
+    return sum;
+}
+
+
 int main()
 {
     struct Sparse s;
+    struct Sparse s2;
 
     create(&s);
+    create(&s2);
+
+    struct Sparse *s3;
+    s3 = add(&s, &s2);
 
     display(s);
+    printf("\n\n");
+    display(s2);
+    printf("\n\n");
+    display((*s3));
+    printf("\n\n");
 }
